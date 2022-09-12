d54bb96e7565b9f7c09c1db35acce203d6af6e9f
majharul <majharuljony163@gmail.com>
2022-09-12 12:20:36 +0600

    add todo projects

--- src/Pages/PortFolio/PortFolio.js
+++ src/Pages/PortFolio/PortFolio.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import Project from '../Project/Project';
+import React, { useEffect, useState } from "react";
+import Project from "../Project/Project";
 
 const PortFolio = () => {
-    // get projects
-    const [projects, setProjects] = useState([]);
-    useEffect(() => {
-        fetch('https://raw.githubusercontent.com/majharul-web/majharul-islam/main/public/projects.json')
-            .then(res => res.json())
-            .then(data => setProjects(data))
-    }, []);
-    return (
-        <div className='bg py-5'>
-            <div className="container">
-                <h3 className='text-center text-decoration-underline pb-5 pt-2'>Recent Works</h3>
-                <div className="row">
-                    {
-                        projects.map(project => <Project key={project.id} project={project}></Project>)
-                    }
-                </div>
-            </div>
+  // get projects
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/majharul-web/majharul-islam/main/public/projects.json")
+      .then((res) => res.json())
+      .then((data) => setProjects(data));
+  }, []);
+  console.log("projects", projects);
+  return (
+    <div className='bg py-5'>
+      <div className='container'>
+        <h3 className='text-center text-decoration-underline pb-5 pt-2'>Recent Works</h3>
+        <div className='row'>
+          {projects.map((project) => (
+            <Project key={project.id} project={project}></Project>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PortFolio;
--- src/Pages/Project/Project.js
+++ src/Pages/Project/Project.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Project = ({ project }) => {
-    const { projectName, projectType, image1, id } = project
-    console.log(project)
-    return (
-        <div className='col-md-4'>
-            <div>
-                <img className='img-fluid' style={{ height: '300px' }} src={image1} alt="not" />
-            </div>
-            <div className='text-center py-3'>
-                <h3>{projectName}</h3>
-                <h5>{projectType}</h5>
-                <Link to={`/projectDetails/${id}`}>
-                    <Button variant='success'>See Details</Button>
-                </Link>
-
-            </div>
-        </div>
-    );
+  const { projectName, projectType, image1, id } = project;
+  return (
+    <div className='col-md-4'>
+      <div style={{ textAlign: "center" }}>
+        <img className='img-fluid' style={{ height: "300px" }} src={image1} alt='not' />
+      </div>
+      <div className='text-center py-3'>
+        <h3>{projectName}</h3>
+        <h5>{projectType}</h5>
+        <Link to={`/projectDetails/${id}`}>
+          <Button variant='success'>See Details</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Project;

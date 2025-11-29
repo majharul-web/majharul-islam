87f745e84f0de454649930a08b277ebf924aa871
majharul-web <majharuljony163@gmail.com>
2025-11-29 17:42:30 +0600

    update projects

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import HomeHead from '../HomeHead/HomeHead';
-import Projects from '../Projects/Projects';
-
+import HomeHead from "../HomeHead/HomeHead";
+import Projects from "../Projects/Projects";
 
 const Home = () => {
-    return (
-        <div>
-            <HomeHead></HomeHead>
-            <Projects></Projects>
-        </div>
-    );
+  return (
+    <div>
+      <HomeHead></HomeHead>
+      <Projects />
+    </div>
+  );
 };
 
 export default Home;
